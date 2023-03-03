9c7a0d2be6056290635b1b43099a47c10f4a998d
22102416 <pedrovs3@hotmail.com>
2023-03-03 09:45:13 -0300

    consumo de cadastro de usuários

--- src/components/Form/Index.tsx
+++ src/components/Form/Index.tsx
@@ -17,6 +17,12 @@ export const Form = () => {
 
 	const [filled, setFilled] = useState(false);
 	const [cepState, setCepState] = useState('');
+	const [genderSelect, setGenderSelect] = useState('dab38ba5-fd71-480a-86ec-5429e5309539')
+	const [cpf, setCpf] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [birthdate, setBirthdate] = useState('');
+	const [nameState, setNameState] = useState('')
 	const [gender, setGender] = useState<genderProps[]>([]);
 	const [addressNumber, setAddressNumber] = useState('');
 	const [complement, setComplement] = useState('');
@@ -24,7 +30,6 @@ export const Form = () => {
 
 
 	useEffect(() => {
-
 		const fetchData = async () => {
 			// get the data from the api
 			const data = await api.get('/gender');
@@ -41,43 +46,6 @@ export const Form = () => {
 
 		console.log(gender)
 	},[])
-
-	const handleSubmitForm = async (e: any) => {
-		e.preventDefault();
-
-		console.log(e.target.firstChild);
-		const formData = new FormData(e.target.firstChild)
-		formData.delete('address[number]')
-		formData.delete('address[postal_code]')
-		// @ts-ignore
-		formData.set('address', { number: addressNumber, postal_code: cepState, complement: complement})
-
-		for (const pair of formData.entries()) {
-			console.log(`${pair[0]}, ${pair[1]}`);
-		}
-
-		const response = await api.post('/user/', formData, {
-			headers: {
-				"Content-Type": "multipart/form-data"
-			}
-		});
-		console.log(response)
-
-		// try {
-		// 	const user = await fetch('http://localhost:3333/user/', {
-		// 		body: formData,
-		// 		method: 'POST',
-		// 	})
-		//
-		// 	console.log(user)
-		//
-		// 	alert('Criado com exito')
-		// } catch (e) {
-		// 	alert(e)
-		// }
-
-	}
-
 
 	const checkCEP = async (e: any) => {
 		setCepState(e.target.value.replace(/\D/g, ''))
@@ -97,39 +65,177 @@ export const Form = () => {
 		}
 	}
 
-	const handleChange = (e) => {
+	const handleSubmitForm = async (e: FormEvent) => {
+		e.preventDefault()
+
+		try {
+			const user = await api.post('/user/', {
+				name: nameState,
+				email: email,
+				password: password,
+				cpf: cpf,
+				birthdate: birthdate,
+				address: {
+					postal_code: cepState,
+					number: addressNumber,
+					complement: complement || null
+				},
+				gender: genderSelect
+			})
+
+			alert(user.data.user)
+		}
+		catch (e) {
+			console.log(e)
+			alert("Houve um erro!")
+		}
+
+	}
+
+	const handleChangeNumber = (e) => {
 		setAddressNumber(e.target.value)
-		setComplement(e.target.value)
 	}
 
 	return (
-			<form name={"cadastro"} className={'w-full h-full pr-6 flex flex-col justify-between'} onSubmit={handleSubmitForm}>
+			<form id={'formRegister'} name={"cadastro"} className={'w-full h-full pr-6 flex flex-col justify-between'} onSubmit={handleSubmitForm}>
 				<form className={"flex flex-col gap-3 pt-24"}>
-					<input id="nome" name={'name'} type="text" className="input bg-white text-black w-full focus:input-bordered focus:input-success capitalize" placeholder="Nome Completo"/>
+					<input id="nome"
+						   name={'name'}
+						   type="text"
+						   className="input bg-white text-black w-full focus:input-bordered focus:input-success capitalize"
+						   placeholder="Nome Completo"
+						   onChange={(e) => setNameState(e.target.value)}
+						   value={nameState}
+					/>
 					<div className="flex flex-2 justify-between">
-						<input id="cpf" name={'cpf'} type="Number" className="input bg-white text-black w-full focus:input-bordered focus:input-success mr-1" placeholder="CPF" required/>
-						<input id="cep" name={'postal_code'} type="text" className="input bg-white text-black w-full focus:input-bordered focus:input-success ml-1" placeholder="CEP (apenas números)" {...register("cep")} onChange={checkCEP} maxLength={11} value={cepState}/>
+						<input
+							id="cpf"
+							name={'cpf'}
+							type="Number"
+							className="input bg-white text-black w-full focus:input-bordered focus:input-success mr-1"
+							placeholder="CPF"
+							onChange={event => setCpf(event.target.value)}
+							value={cpf}
+							required
+						/>
+						<input
+							id="cep"
+							name={'postal_code'}
+							type="text"
+							className="input bg-white text-black w-full focus:input-bordered focus:input-success ml-1"
+							placeholder="CEP (apenas números)" {...register("cep")}
+							onChange={checkCEP}
+							maxLength={11}
+							value={cepState}
+						/>
 					</div>
-					<input id="email" name={'email'} type="email" className="input bg-white text-black w-full focus:input-bordered focus:input-success" placeholder="E-mail"/>
-					<input id="senha" name={'password'} type="password" className="input bg-white text-black w-full focus:input-bordered focus:input-success" placeholder="Senha"/>
-					<input id="nascimento" name={'birthdate'} type="date" className="input bg-white text-black w-full focus:input-bordered focus:input-success placeholder-blue-500" placeholder="Data de Nascimento"/>
-					<select className="select w-full max-w-xs" name={'gender'}>
-						{ gender.map((gender) => (<option key={gender.id} value={gender.id} className={'input'}>{gender.name}</option>)) }
+					<input
+						id="email"
+						name={'email'}
+						type="email"
+						className="input bg-white text-black w-full focus:input-bordered focus:input-success"
+						placeholder="E-mail"
+						onChange={event => setEmail(event.target.value)}
+						value={email}
+					/>
+					<input
+						id="senha"
+						name={'password'}
+						type="password"
+						className="input bg-white text-black w-full focus:input-bordered focus:input-success"
+						placeholder="Senha"
+						onChange={event => setPassword(event.target.value)}
+						value={password}
+					/>
+					<input
+						id="nascimento"
+						name={'birthdate'}
+						type="date"
+						className="input bg-white text-black w-full focus:input-bordered focus:input-success placeholder-blue-500"
+						placeholder="Data de Nascimento"
+						onChange={event => setBirthdate(event.target.value)}
+						value={birthdate}
+					/>
+					<select
+						className="select w-full max-w-xs"
+						name={'gender'}
+						onChange={event => console.log(setGenderSelect(event.target.value))}
+					>
+						{
+							gender.map((gender) => (
+									<option key={gender.id} value={gender.id} className={'input'}>{gender.name}</option>
+								)
+							)
+						}
 					</select>
-					<input id="estado" type="text" className="input bg-white text-black w-full focus:input-bordered focus:input-success" placeholder="Estado" {...register("uf" )} disabled={filled}/>
+					<input
+						id="estado"
+						type="text"
+						className="input bg-white text-black w-full focus:input-bordered focus:input-success"
+						placeholder="Estado"
+						{...register("uf" )}
+						disabled={filled}
+					/>
 					<div className="flex flex-2 justify-between">
-						<input id="cidade" type="text" className="input bg-white text-black w-full focus:input-bordered focus:input-success mr-1" placeholder="Cidade" {...register("city" )} disabled={filled}/>
-						<input id="bairro" type="text" className="input bg-white text-black w-full focus:input-bordered focus:input-success ml-1" placeholder="Bairro" {...register("neighborhood" )} disabled={filled}/>
+						<input
+							id="cidade"
+							type="text"
+							className="input bg-white text-black w-full focus:input-bordered focus:input-success mr-1"
+							placeholder="Cidade"
+							{...register("city" )}
+							disabled={filled}
+						/>
+						<input
+							id="bairro"
+							type="text"
+							className="input bg-white text-black w-full focus:input-bordered focus:input-success ml-1"
+							placeholder="Bairro"
+							{...register("neighborhood" )}
+							disabled={filled}
+						/>
 					</div>
 					<div className="flex flex-2 justify-between">
-						<input id="rua" type="text" className="input bg-white text-black w-full focus:input-bordered focus:input-success mr-1" placeholder="Rua"  {...register("address" )} disabled={filled}/>
-						<input id="numero" name={'address[number]'} type="text" className="input bg-white text-black w-full focus:input-bordered focus:input-success ml-1" placeholder="Número" onChange={handleChange} value={addressNumber} />
+						<input
+							id="rua"
+							type="text"
+							className="input bg-white text-black w-full focus:input-bordered focus:input-success mr-1"
+							placeholder="Rua"
+							{...register("address" )}
+							disabled={filled}
+						/>
+						<input
+							id="numero"
+							name={'address[number]'}
+							type="text"
+							className="input bg-white text-black w-full focus:input-bordered focus:input-success ml-1"
+							placeholder="Número"
+							onChange={handleChangeNumber}
+							value={addressNumber}
+						/>
 					</div>
-					<input id="complemento" name={'address[complement]'} type="text" className="input bg-white text-black w-full focus:input-bordered focus:input-success" placeholder="Complemento" onChange={handleChange} value={complement} />
+					<input
+						id="complemento"
+						name={'address[complement]'}
+						type="text"
+						className="input bg-white text-black w-full focus:input-bordered focus:input-success"
+						placeholder="Complemento"
+						onChange={event => setComplement(event.target.value)}
+						value={complement}
+					/>
 				</form>
 				<div className="flex flex-col gap-3 items-end pb-56">
-					<button className={"btn btn-primary w-1/4 rounded-full bg-turquoise-700 border-0 text-xl text-black hover:bg-blue-600 hover:text-white"} type="submit">Enviar</button>
-					<button className={"btn btn-accent w-1/8 rounded-full bg-tufts-blue px-6 border-0 text-l text-black hover:bg-blue-600 hover:text-white"} type="submit"><p><NavLink to={'/login'}>Sou uma ONG</NavLink></p></button>
+					<button
+						className={"btn btn-primary w-1/4 rounded-full bg-turquoise-700 border-0 text-xl text-black hover:bg-blue-600 hover:text-white"}
+						type="submit"
+					>
+						Enviar
+					</button>
+					<button
+						className={"btn btn-accent w-1/8 rounded-full bg-tufts-blue px-6 border-0 text-l text-black hover:bg-blue-600 hover:text-white"}
+						type="submit"
+					>
+						<p><NavLink to={'/login'}>Sou uma ONG</NavLink></p>
+					</button>
 				</div>
 			</form>
 	)
