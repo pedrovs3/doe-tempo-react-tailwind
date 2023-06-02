b98fbf55721b366f7b670f52db3e57b4b681042d
22102416 <pedrovs3@hotmail.com>
2023-06-02 08:12:36 -0300

    fixing login screen

--- src/components/HeaderLogin/Index.tsx
+++ src/components/HeaderLogin/Index.tsx
@@ -4,14 +4,14 @@ import {Link} from "react-router-dom";
 
 export function Header() {
     return (
-        <div className={'h-full w-full flex  justify-between'}>
+        <div className={'h-full w-full flex justify-evenly flex-col pl-10'}>
             <Link to={'/'}>
             <button className={'btn w-40 rounded-full bg-turquoise-700 border-0 text-white flex justify-center hover:bg-accent'} type={'submit'}>
                 <CaretLeft size={32} className={'text-blueberry'} />
                 <p className={'text-blueberry'}>Voltar</p>
             </button>
             </Link>
-            <img src={AuthImg} className={'align-center justify-start'} alt={'image people authenthication'}/>
+            <img src={AuthImg} className={'align-center justify-center'} alt={'image people authenthication'}/>
         </div>
 
     )
