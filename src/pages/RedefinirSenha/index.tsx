81d05dad9f12a823338c4296376129828cc4921f
Pedro <pedrovs3@hotmail.com>
2023-06-06 15:58:00 -0300

    fix update password

--- src/pages/RedefinirSenha/index.tsx
+++ src/pages/RedefinirSenha/index.tsx
@@ -29,7 +29,7 @@ export default function RedefinirSenha() {
         reloaded = true
         if (!reloaded) {
 
-            window.location.reload(true)
+            window.location.reload()
         }
     }, [])
 
