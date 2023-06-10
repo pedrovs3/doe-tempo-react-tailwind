bd54117da9470518d60a3806558ccb8e66e0d717
Pedro <pedrovs3@hotmail.com>
2023-06-09 23:17:15 -0300

    add errorElement on dashboards

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -9,14 +9,13 @@ export default function PageNotFound() {
             <img src={NotFoundSvg} alt={"Not found image"} className={"h-1/4"}/>
             <div className={'flex flex-col items-center justify-center'}>
                 <h1 className={'text-8xl font-bold text-blueberry'}>404</h1>
-                <h1 className={'text-5xl font-medium text-blueberry'}>Página nao encontrada!</h1>
+                <h1 className={'text-5xl font-medium text-blueberry'}>Página não encontrada!</h1>
             </div>
             <NavLink to={'/'}>
                 <button className={'btn bg-blueberry border-0 hover:bg-little-white hover:text-blueberry'}>
                         Voltar para tela inicial
                 </button>
             </NavLink>
-
         </div>
     )
 }
